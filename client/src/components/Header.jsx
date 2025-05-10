a54a89f272ce13e40a00204bbbd93cdab7ff33f0
muhammedmanduz <muhammed.manduz@gmail.com>
2025-05-10 22:16:04 +0300

    create user model

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="bg-slate-200">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link className="font-bold" to="/">
+          Auth App
+        </Link>
+
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+
+          <Link to="/sign-in">
+            <li>Sign In</li>
+          </Link>
+
+          <Link to="/sign-up">
+            <li>Sign Up</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
