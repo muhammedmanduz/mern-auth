9c27ef96c742345e90dd650aabe38d64e8d1e31e
muhammedmanduz <muhammed.manduz@gmail.com>
2025-05-10 23:42:35 +0300

    create test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+import userRoutes from "./routes/user.route.js";
 
 mongoose
   .connect(process.env.MONGO)
@@ -17,3 +18,6 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
+
+// API routes
+app.use("/api/user", userRoutes);
