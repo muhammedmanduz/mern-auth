50d6d75af4129f4ca5cd27d7cc0f63713ae0c389
muhammedmanduz <muhammed.manduz@gmail.com>
2025-05-12 14:42:09 +0300

    complete sign up page ui

--- client/src/pages/SingnUp.jsx
+++ client/src/pages/SingnUp.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SingnUp = () => {
-  return <div>SingnUp</div>;
+  return (
+    <div className="p-3  max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign up</h1>
+
+      <form className="flex flex-col gap-4 ">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg mb-4"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg mb-4"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 text-white  p-3 rounded-lg uppercase hover:opacity-95 disabled::opacity-80">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link>
+          <span className="text-blue-500">Sing in</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SingnUp;
