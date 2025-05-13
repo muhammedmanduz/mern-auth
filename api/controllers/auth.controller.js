9427618f6284ca299044b1cbc9dd6537182f1f65
muhammedmanduz <muhammed.manduz@gmail.com>
2025-05-13 16:33:41 +0300

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,5 +15,34 @@ export const signup = async (req, res, next) => {
     });
   } catch (error) {
     next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET,
+      { expiresIn: "5d" }
+    );
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expireDate = new Date(Date.now() + 3600000); // 1 hour
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: expireDate,
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    return next(errorHandler(500, "Internal Server Error"));
   }
 };
