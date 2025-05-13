54b2255e782ad26cbc27cd1301e748e1b9d2a59d
muhammedmanduz <muhammed.manduz@gmail.com>
2025-05-13 17:30:44 +0300

    complete the sign in page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,80 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
-const SignIn = () => {
-  return <div>SignIn</div>;
+const SingnIn = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="p-3  max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg mb-4"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white  p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont Have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-500">Sing Up</span>
+        </Link>
+        <p className="text-red-500 mb-5">{error && "Something went wrong!"}</p>
+      </div>
+    </div>
+  );
 };
 
-export default SignIn;
+export default SingnIn;
--- client/src/pages/SingnUp.jsx
+++ client/src/pages/SingnUp.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { set } from "mongoose";
 
 const SingnUp = () => {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -32,6 +34,7 @@ const SingnUp = () => {
         setError(true);
         return;
       }
+      navigate("/sign-in");
     } catch (error) {
       setLoading(false);
       setError(true);
@@ -67,17 +70,17 @@ const SingnUp = () => {
 
         <button
           disabled={loading}
-          className="bg-slate-700 text-white  p-3 rounded-lg uppercase hover:opacity-95 disabled::opacity-80"
+          className="bg-slate-700 text-white  p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          Sign Up
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
-        <Link>
+        <Link to={"/sign-in"}>
           <span className="text-blue-500">Sing in</span>
         </Link>
-        <p className="text-red-500 mb-5">{error && "Something went wrong!"}</p>
+        <p className="text-red-700 mb-5">{error && "Something went wrong!"}</p>
       </div>
     </div>
   );
