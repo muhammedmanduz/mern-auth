c28bd585c5f91a8a0cd96383be602660f06efce1
muhammedmanduz <muhammed.manduz@gmail.com>
2025-05-14 17:54:00 +0300

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,26 @@
+// store.js
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage için
+// veya sessionStorage için:
+// import storageSession from 'redux-persist/lib/storage/session'
+
 import userReducer from "./user/userSlice";
 
+// Persist konfigürasyonu
+const persistConfig = {
+  key: "root", // BU ANAHTAR ZORUNLUDUR
+  storage,
+  // Sadece belirli reducer'ları persist yapmak isterseniz:
+  // whitelist: ['user']
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
 export const store = configureStore({
-  reducer: { user: userReducer },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+  reducer: {
+    user: persistedReducer,
+  },
 });
 
-export default store;
+export const persistor = persistStore(store);
